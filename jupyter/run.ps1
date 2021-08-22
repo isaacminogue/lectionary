f7d6de2fe98cc2cffef4fd1bf252f058ca4527fe
Isaac Minogue <isaacminogue@Mac.fritz.box>
2021-08-23 08:09:34 +1200

    rejig for publishing

--- jupyter/run.ps1
+++
@@ -1,3 +0,0 @@
-docker run --rm --name lectionary-jupyter -p 8888:8888 -v c:\data\Lectionary\jupyter:/home/jovyan/work jupyter/base-notebook;
-
-docker kill lectionary-jupyter;
--- website/README.md
+++
@@ -1,7 +0,0 @@
-# TODO list
-
-1. Get permission
-2. About section
-3. Larger breakpoint designs?
-4. Little tidyups (like more icons etc)
-5. Put it somewhere?
